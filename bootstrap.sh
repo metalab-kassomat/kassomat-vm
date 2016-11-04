b41f48943938b4d1c23042619dd067ed97a97b97
sixtyeight <christian_sl@gmx.at>
2016-11-04 21:14:31 +0100

    create /etc/apt/sources.list.d/kassomat.list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # openjdk-8-jdk is only available via backports in jessie
-echo "bootstrap: adding http://ftp.de.debian.org/debian to /etc/apt/sources.list"
-echo "deb http://ftp.de.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
+echo "bootstrap: creating /etc/apt/sources.list.d/kassomat.list"
+echo "deb http://ftp.de.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/kassomat.list 
 
 # install ansible which is used for all other provisioning of the vm
 echo "bootstrap: installing ansible"
