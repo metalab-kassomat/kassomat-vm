c660850246ceaa9f1328da5f12770a72062d0101
sixtyeight <christian_sl@gmx.at>
2016-11-06 22:02:37 +0100

    update git repos

--- init_gits.sh
+++ init_gits.sh
@@ -4,17 +4,17 @@ rm -rf gits 2> /dev/null
 mkdir gits
 cd gits
 
-echo -e "\e[92mcloning Changeomatic repo\e[0m"
-git clone https://github.com/sixtyeight/changeomatic
+echo -e "\e[92mcloning kassomat-changeomatic repo\e[0m"
+git clone https://github.com/metalab-kassomat/kassomat-changeomatic
 echo
 
 echo -e "\e[92mcloning kassomat-scripts repo\e[0m"
-git clone https://github.com/phaer/kassomat-scripts
+git clone https://github.com/metalab-kassomat/kassomat-scripts
 echo
 
-echo -e "\e[92mcloning Payout repo\e[0m"
-git clone https://github.com/sixtyeight/Payout
+echo -e "\e[92mcloning kassomat-payout repo\e[0m"
+git clone https://github.com/metalab-kassomat/kassomat-payout
 echo
 
-echo -e "\e[92mcloning PayoutSimulator repo\e[0m"
-git clone https://github.com/hettipeti/PayoutSimulator
+echo -e "\e[92mcloning kassomat-payout-simulator repo\e[0m"
+git clone https://github.com/metalab-kassomat/kassomat-payout-simulator
