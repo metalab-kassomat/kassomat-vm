91d37ed20b3c254757021e64170b592b0ffdeb89
sixtyeight <christian_sl@gmx.at>
2016-11-11 22:37:39 +0100

    fixed repo dirs

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,13 @@
 cd gits
 
 echo -e "\e[92mBuilding Payout\e[0m"
-(cd Payout; make clean; make)
+(cd kassomat-payout; make clean; make)
 echo
 
 echo -e "\e[92mBuilding PayoutSimulator\e[0m"
-(cd PayoutSimulator; ./build.sh)
+(cd kassomat-payout-simulator; ./build.sh)
 echo
 
 echo -e "\e[92mBuilding Changeomatic\e[0m"
-(cd changeomatic; ./build.sh)
+(cd kassomat-changeomatic; ./build.sh)
 
